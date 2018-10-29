e61dd09dca6bc4aae9c24b84a249a8006c316e78
Markus <32583386+InnerlohingerMarkus@users.noreply.github.com>
2018-10-29 10:32:43 +0100

    evaluation implementation
    implementation of reading Images, extracting the grayscale values into a 2D-Array, Walking through all subfolders of a given Folder and returning their images

--- src/core/evaluation/Evaluation.java
+++ src/core/evaluation/Evaluation.java
@@ -1,8 +1,13 @@
 package core.evaluation;
 
+import java.util.ArrayList;
 import java.util.HashMap;
-
 import utils.Debug;
+
+import java.io.IOException;
+import java.nio.file.FileVisitOption;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 /**
  * TODO description
@@ -15,29 +20,47 @@ public class Evaluation {
 	/**
 	 * Returns evaluated data based on incoming image data
 	 * TODO implement
+	 * @throws IOException 
 	 */
-	public static EvaluationDataSet get(EvaluationDataSet data) {
+	public static EvaluationDataSet get(EvaluationDataSet data) throws IOException {
 		// get data:
 		String name = data.getName();
 		String imagePath = data.getImagePath();
 		String sourceFolder = data.getSourceFolder();
-		String kFactor = data.getKFactor();
-		String heuristic = data.getHeuristic();
+		int kFactor = Integer.parseInt(data.getKFactor());
+		int heuristic = Integer.parseInt(data.getHeuristic());
 		
 		Debug.log("-> EvaluationDataSet values:");
 		Debug.log(data.toString());
 		
+		ImageReader ir = new ImageReader(imagePath);
+		Pathfinder pf = new Pathfinder(sourceFolder);
 		
-		// create entries:
-		EvaluationDataSetEntry entry1 = new EvaluationDataSetEntry("fileFolderPath", "fileName", "fileExtension", new int[][] {{8, 8, 8, 10, 10}, {8, 9, 9, 9, 9}});
+		ArrayList<String> paths = pf.getk_paths(kFactor);
+		EvaluationDataSetEntry entry;
+		
+		//adds the selected image to the dataset
+		entry = new EvaluationDataSetEntry(ir.getImagepath(),ir.getImagename(),ir.getImageextension(), ir.convertTo2DArray());
+		data.addEntry(entry);
+		
+		
+		//adds k-many images from the database to the dataset
+		for(int i=0; i<kFactor;i++) {
+			ImageReader temp = new ImageReader(paths.get(i));
+			entry = new EvaluationDataSetEntry(paths.get(i), pf.getFilenames().get(i), pf.getExtensions().get(i), temp.convertTo2DArray());
+			data.addEntry(entry);
+		}
 		// you can either use the constructor to pass the entry arguments,
 		// or use the implemented setter methods e.g.:
 		// entry1.setGreyScaleValue(...);
 		
-		// set entries:
-		//data.addEntry(entry1);
+
 		
 		// return the set, filled with all the entries and values:
 		return data;
 	}
+	
+	
+	
+	
 }
---
+++ src/core/evaluation/ImageReader.java
@@ -0,0 +1,126 @@
+package core.evaluation;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.awt.image.BufferedImage;
+import java.awt.image.Raster;
+import javax.imageio.ImageIO;
+
+
+public class ImageReader {
+	private BufferedImage image;
+	private String imagepath, imagename, imageextension;
+
+	
+	ImageReader(){
+		this.image = null;
+		this.setImagepath(null);
+		this.setImagename(null);
+		this.setImageextension(null);
+	}
+	
+	ImageReader(String imagesource){
+		Path path = Paths.get(imagesource);
+		
+		this.imagename = path.getFileName().toString();
+		this.imagepath = imagesource.substring(0, imagesource.length()-imagename.length());
+		
+		if(imagesource.contains(".jpg"))
+			this.imageextension = "jpg";
+		else if(imagesource.contains(".png"))
+			this.imageextension = "png";
+		else if(imagesource.contains(".bmp"))
+			this.imageextension = "bmp";
+		
+		try{
+			this.image = new BufferedImage(1, 1, BufferedImage.TYPE_BYTE_GRAY);
+			this.image = ImageIO.read(new File(imagesource));
+			System.out.println("Reading complete.");	
+			
+		}catch(IOException e){
+			System.out.println("Error: "+e);
+		}
+		
+	}
+		
+	public void read(String imagesource) {
+	try{
+		this.image = new BufferedImage(1, 1, BufferedImage.TYPE_BYTE_GRAY);
+		
+		this.image = ImageIO.read(new File(imagesource));
+		
+		System.out.println("Reading complete.");	
+		
+	}catch(IOException e){
+		System.out.println("Error: "+e);
+	}
+	}
+	
+	int [][] convertTo2DArray(){
+		int height = image.getHeight();
+		int width= image.getWidth();
+		int[][] result = new int [width][height];
+		Raster raster = image.getData();
+		
+		for(int col = 0; col < height; col++) {
+			for(int row = 0; row < width; row++) {
+				result[row][col] = raster.getSample(row,col,0);
+			}	
+		}
+		return result;	
+	}
+	
+	public void printArray() {
+		int[][]array = this.convertTo2DArray();
+		for(int col=0; col<array[0].length; col++) {
+			for(int row=0; row<array.length; row++) {
+				System.out.print(array[row][col] + " ");
+			}
+			System.out.println();
+		}
+	}
+	
+	public void write() {
+	    try{
+	        ImageIO.write(image, "jpg", new File("output.jpg"));
+	        System.out.println("Writing complete.");
+	      }catch(IOException e){
+	        System.out.println("Error: "+e);
+	      }
+	}
+	
+	
+	public int imgHeight() {
+		return image.getHeight();
+	}
+	
+	public int imgWidth() {
+		return image.getWidth();
+	}
+
+	public String getImagepath() {
+		return imagepath;
+	}
+
+	public void setImagepath(String imagepath) {
+		this.imagepath = imagepath;
+	}
+
+	public String getImagename() {
+		return imagename;
+	}
+
+	public void setImagename(String imagename) {
+		this.imagename = imagename;
+	}
+
+	public String getImageextension() {
+		return imageextension;
+	}
+
+	public void setImageextension(String imageextension) {
+		this.imageextension = imageextension;
+	}
+	
+}
