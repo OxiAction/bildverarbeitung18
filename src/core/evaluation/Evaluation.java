f7227b18c419f8a86453542e75af8b40559eae28
Michael Schreiber <schreiber.gausburg@web.de>
2018-11-17 01:30:55 +0100

    code formatting - added descriptions

--- src/core/evaluation/Evaluation.java
+++ src/core/evaluation/Evaluation.java
@@ -9,30 +9,28 @@ import utils.Debug;
 import java.io.IOException;
 
 /**
- * TODO description
- * 
- * @author 
- *
+ * The Evaluation class is responsible for processing every entry in a set.
  */
 public class Evaluation {
-	
+
 	/**
-	 * Returns evaluated data based on incoming set
+	 * Returns evaluated set based on incoming set.
+	 * Threads (EvaluationThread) are used to speed up the calculation process (e.g. for grey scale / histogram data gathering).
+	 * After all threads have finished their work, we get the distance of the metric (based histogram datas) for each entry.
+	 * The k-factor determines how many "nearest" (which means "similar distance") entries are stored within an entry.
 	 * 
-	 * @throws IOException 
+	 * @throws IOException
 	 */
 	public static EvaluationDataSet get(EvaluationDataSet set) throws IOException {
 		Debug.log("-> EvaluationDataSet values:");
 		Debug.log(set.toString());
-		
+
 		String sourceFolder = set.getSourceFolder();
-		Debug.log("Set -> sourceFolder: " + sourceFolder);
-		
-		
+
 		ArrayList<String> paths = PathFinder.getPaths(sourceFolder);
-		
+
 		ArrayList<EvaluationThread> evaluationThreads = new ArrayList<EvaluationThread>();
-		
+
 		int idCounter = 1;
 		for (int j = 0; j < paths.size(); j++) {
 			String absoluteFilePath = paths.get(j);
@@ -40,79 +38,73 @@ public class Evaluation {
 				EvaluationThread evaluationThread = new EvaluationThread(idCounter, absoluteFilePath, set);
 				evaluationThreads.add(evaluationThread);
 				evaluationThread.start();
-				
+
 				idCounter++;
 			}
 		}
-		
+
 		for (EvaluationThread evaluationThread : evaluationThreads) {
 			try {
 				evaluationThread.join();
 			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
-		
+
 		for (EvaluationThread evaluationThread : evaluationThreads) {
 			set.addEntry(evaluationThread.getEntry());
 		}
-		
-		// TODO [Markus] leave one out cross validation: compare each entry with all other entries (by metric) and set k-nearest entries ids in entry
+
+		// leave one out cross validation: compare each entry with all other entries (by metric) and set k-nearest entries ids in entry
 		List<EvaluationDataSetEntry> setEntries = set.getEntries();
 		for (EvaluationDataSetEntry entry : setEntries) {
 			int[] histogramData = entry.getHistogramData();
 			int kFactor = Integer.parseInt(set.getKFactor());
 			Hashtable<EvaluationDataSetEntry, Double> kNearest = new Hashtable<EvaluationDataSetEntry, Double>();
 			int count = 0;
-			// INNER FOR LOOP (compare all others with current entry):			
+
+			// compare all others with current entry	
 			for (EvaluationDataSetEntry innerEntry : setEntries) {
-				if(entry == innerEntry)
+				if (entry == innerEntry) {
 					continue;
-				
+				}
+
 				int[] innerHistogramData = innerEntry.getHistogramData();
-				
-			
+
 				// compare histogramData of two entries (set.getMetricName() is the name which was choosen in the new scan metric name dropdown):
-				//Metric.getDataByName(set.getMetricName(), histogramData, histogramData_OF_ENTRY_IN_INNER_FOR_LOOP);
 				double distance = Metric.getDataByName(set.getMetricName(), histogramData, innerHistogramData);
-				
-				
-				//adds a distance to the k-nearest hashtable:
-				//first we check if we already have k-elements
-				//when there are already k-elements in the hashtable, we replace the largest with the new one (if it is smaller)
-				if(count < kFactor) {
+
+				// adds a distance to the k-nearest hashtable:
+				// first we check if we already have k-elements
+				// when there are already k-elements in the hashtable, we replace the largest with the new one (if it is smaller)
+				if (count < kFactor) {
 					kNearest.put(innerEntry, distance);
 					count++;
-				}else {
-					//finding the largest element in the hashtable
-					Object maxKey=null;
-					Double maxValue = Double.MIN_VALUE; 
-					for(Map.Entry<EvaluationDataSetEntry,Double> mapEntry : kNearest.entrySet()) {
-					     if(mapEntry.getValue() > maxValue) {
-					         maxValue = mapEntry.getValue();
-					         maxKey = mapEntry.getKey();
-					     }  
+				} else {
+					// finding the largest element in the hashtable
+					Object maxKey = null;
+					Double maxValue = Double.MIN_VALUE;
+					for (Map.Entry<EvaluationDataSetEntry, Double> mapEntry : kNearest.entrySet()) {
+						if (mapEntry.getValue() > maxValue) {
+							maxValue = mapEntry.getValue();
+							maxKey = mapEntry.getKey();
+						}
 					}
-					//replacing the largest element with the actual one (if it is smaller)
-					if(distance < maxValue) {
+					// replacing the largest element with the actual one (if it is smaller)
+					if (distance < maxValue) {
 						kNearest.remove(maxKey);
 						kNearest.put(innerEntry, distance);
 					}
 				}
-				
-				
-				// add id as a kNearest to (OUTER FOR LOOP) entry (in case it is actually k-nearest):
-				//entry.addKNearestByID(id_OF_ENTRY_IN_INNER_FOR_LOOP);
-				for(Map.Entry<EvaluationDataSetEntry,Double> mapEntry : kNearest.entrySet())
-					entry.addKNearestByID(mapEntry.getKey().getID());
-				
-			// END INNER FOR LOOP
 
+				// add id as a kNearest to entry (in case it is actually k-nearest):
+				for (Map.Entry<EvaluationDataSetEntry, Double> mapEntry : kNearest.entrySet()) {
+					entry.addKNearestByID(mapEntry.getKey().getID());
+				}
 			}
-			
+
 		}
-		
+
 		// return the set, filled with all the entries and values:
 		return set;
 	}
--- src/core/evaluation/Metric.java
+++ src/core/evaluation/Metric.java
@@ -7,70 +7,80 @@ public class Metric {
 	public static final String NAME_BHATTACHARYY_DISTANCE = "Bhattacharyya Distance";
 
 	protected static final String[] NAMES = { NAME_CORRELATION, NAME_CHI_SQUARE, NAME_INTERSECTION, NAME_BHATTACHARYY_DISTANCE };
-	
+
 	/**
+	 * returns value of two histograms, based on correlation metric
 	 * 
-	 * @param args
+	 * @param h1
+	 * @param h2
 	 * @return
 	 */
 	public static double getCorrelationData(int[] h1, int[] h2) {
-		double n=0, d1=0, d2=0;
+		double n = 0, d1 = 0, d2 = 0;
 		double h1avg = getAverage(h1);
 		double h2avg = getAverage(h2);
-		
-		for(int i=0;i<h1.length;i++) {
-			n += (h1[i]-h1avg)*(h2[i]-h2avg);
-			d1 += Math.pow((h1[i]-h1avg), 2);
-			d2 += Math.pow((h2[i]-h2avg), 2);
-		}		
-		return (n/Math.sqrt(d1*d2));
+
+		for (int i = 0; i < h1.length; i++) {
+			n += (h1[i] - h1avg) * (h2[i] - h2avg);
+			d1 += Math.pow((h1[i] - h1avg), 2);
+			d2 += Math.pow((h2[i] - h2avg), 2);
+		}
+		return (n / Math.sqrt(d1 * d2));
 	}
-	
+
 	/**
+	 * returns value of two histograms, based on chi-square metric
 	 * 
-	 * @param args
+	 * @param h1
+	 * @param h2
 	 * @return
 	 */
 	public static double getChiSquareData(int[] h1, int[] h2) {
 		double d = 0;
-		for(int i=0; i<h1.length;i++) {
-			d += (Math.pow((h1[i]-h2[i]), 2)) / h1[i];
-		}		
+		for (int i = 0; i < h1.length; i++) {
+			d += (Math.pow((h1[i] - h2[i]), 2)) / h1[i];
+		}
 		return d;
 	}
-	
+
 	/**
+	 * returns value of two histograms, based on intersection metric
 	 * 
-	 * @param args
+	 * @param h1
+	 * @param h2
 	 * @return
 	 */
 	public static double getIntersectionData(int[] h1, int[] h2) {
 		double d = 0;
-		for(int i = 0; i < h1.length; i++)
+		for (int i = 0; i < h1.length; i++)
 			d += Math.min(h1[i], h2[i]);
-		
+
 		return d;
 	}
-	
+
 	/**
+	 * returns value of two histograms, based on bhattacharyya distance metric
 	 * 
-	 * @param args
+	 * @param h1
+	 * @param h2
 	 * @return
 	 */
 	public static double getBhattacharyyaDistanceData(int[] h1, int[] h2) {
 		double s = 0, q;
-		for (int i = 0; i < h1.length; i++) 
-			s += Math.sqrt(h1[i]*h2[i]);
-			
-		q = 1 / (Math.sqrt(getAverage(h1)*getAverage(h2)*Math.pow(h1.length, 2)));
-			
-		return Math.sqrt(1 - q * s);	
+		for (int i = 0; i < h1.length; i++)
+			s += Math.sqrt(h1[i] * h2[i]);
+
+		q = 1 / (Math.sqrt(getAverage(h1) * getAverage(h2) * Math.pow(h1.length, 2)));
+
+		return Math.sqrt(1 - q * s);
 	}
-	
+
 	/**
 	 * get metric data by metric name
 	 * 
-	 * @param args
+	 * @param name
+	 * @param h1
+	 * @param h2
 	 * @return
 	 */
 	public static double getDataByName(String name, int[] h1, int[] h2) {
@@ -87,7 +97,7 @@ public class Metric {
 			throw new Error("Unknwon metric Name: " + name);
 		}
 	}
-	
+
 	/**
 	 * returns all available metric names as array
 	 * 
@@ -96,13 +106,18 @@ public class Metric {
 	public static String[] getNames() {
 		return NAMES;
 	}
-	
-	//calculates the average of histogram
-	private static double getAverage(int[]histogram) {
+
+	/**
+	 * calculates the average of histogram
+	 * 
+	 * @param histogram
+	 * @return
+	 */
+	protected static double getAverage(int[] histogram) {
 		int avg = 0;
-		for(int i=0; i<histogram.length;i++) 
-			avg+=histogram[i];
-		return (avg/histogram.length);
-		
+		for (int i = 0; i < histogram.length; i++)
+			avg += histogram[i];
+		return (avg / histogram.length);
+
 	}
 }
